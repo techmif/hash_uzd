f1022673af45acc91020a1a6cf568c95fa7f861c
supermario9000 <lamporest@gmail.com>
2025-09-16 11:00:44 +0300

    anti utf8 nuskaitymas
    pradėjau rašyti nuskaitymo funkciją, kuri apeitų utf8 apribojimus. T.y., man nereikės viso projekto encodingo keisti į utf8

--- lib.hpp
+++ lib.hpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <bitset>
+#include <fstream>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
+using std::bitset;
+using std::ios;
+using std::ifstream;
+using std::vector;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "lib.hpp"
 
+void CharIBinary(string input){
+    char simbolis;
+    for(int i=0; i<=input.length(); i++)
+    {
+        simbolis = input[i];
+        cout<< simbolis << " ";
+        cout<< bitset<8>(simbolis) << " ";
+    }
+    cout<<endl;
+}
+
 void KonvertCharIAscii (string input){
     char simbolis;
     for(int i=0; i<=input.length(); i++)
@@ -8,13 +19,36 @@ void KonvertCharIAscii (string input){
         cout<< simbolis << " ";
         cout<< (int)simbolis << " ";
     }
+    cout<<endl;
+}
+
+void nuskaitytimp3(vector<int> &nuskaityti_binary_duomenys){
+    ifstream in("irasas.mp3", ios::binary); //uzdedu binary input output flag, kad nuskaitomo failo nereiketu uzkoduoti i utf8. radau info https://cplusplus.com/doc/tutorial/files/
+    if(!in){
+        cout<<"Failas nerastas"<<endl;
+        return;
+    }
+    else 
+    {
+        cout<<"nuskaitoma is failo.";
+        for(char byte; in.get(byte);){
+            nuskaityti_binary_duomenys.push_back((unsigned char)byte);
+        }
+    }
+    in.close();
 }
 
 int main (){
-    cout << "Iveskite zodi, kuri norite uzkoduoti: " <<endl;
+    vector<int> nuskaityti_binary_duomenys;
+
+    /* cout << "Iveskite zodi, kuri norite uzkoduoti: " <<endl;
     string input;
     cin >> input;
     cout<< input<<endl;
     KonvertCharIAscii(input);
+    CharIBinary(input); */
+    nuskaitytimp3(nuskaityti_binary_duomenys);
+    cout<<"Pirmi trys simboliai: "<< nuskaityti_binary_duomenys[2]<<" "<<nuskaityti_binary_duomenys[3]<<" "<<nuskaityti_binary_duomenys[4]<<endl;
+    cout<<"Vektoriaus dydis: "<<nuskaityti_binary_duomenys.size()<<endl;
     return 0;
 }
